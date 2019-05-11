c8225c17b6353a8a01c48076669c2548a6e9dc89
Mookie299 <daniellabillini@gmail.com>
2019-05-11 16:00:44 -0400

    yur

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-
+$("#leftButton").click("getRandomPokemonLeft")
 //starter function to pull a random pokemon from the PokeAPI
   function getRandomPokemonLeft() {
     let randomNumber = Math.floor(Math.random() * 800) + 1;
@@ -12,9 +12,14 @@ $(document).ready(function(){
 
 
 //all code here
+$("#leftButton").click("getRandomPokemonRight")
 
-
-
+function getRandomPokemonRight() {
+    let randomNumber = Math.floor(Math.random() * 800) + 1;
+    $.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then(function(pokeData){
+      console.log("Left Pokemon Data: ", pokeData);
+    });
+  }
 
 
 })
